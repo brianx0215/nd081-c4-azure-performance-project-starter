04f4e618b9beb99554a6e59e3e69771d96fdb9a0
brianx0215 <brianxu0215@gmail.com>
2021-07-16 04:11:27 +0100

    Add missing imports

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -8,17 +8,19 @@ import logging
 from datetime import datetime
 
 # App Insights
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.log_exporter import AzureEventHandler
 from opencensus.ext.azure import metrics_exporter
 from opencensus.stats import aggregation as aggregation_module
 from opencensus.stats import measure as measure_module
 from opencensus.stats import stats as stats_module
 from opencensus.stats import view as view_module
 from opencensus.tags import tag_map as tag_map_module
+from opencensus.trace import config_integration
 from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace.tracer import Tracer
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
-from opencensus.trace.samplers import ProbabilitySampler
 
 # Logging
 config_integration.trace_integrations(['logging'])
@@ -33,6 +35,9 @@ logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=1a9b0f
 logger.setLevel(logging.INFO)
 
 # Metrics
+stats = stats_module.stats
+view_manager = stats.view_manager
+
 exporter = metrics_exporter.new_metrics_exporter(
 enable_standard_metrics=True,
 connection_string='InstrumentationKey=1a9b0f8a-9754-4cf0-8817-b241762feaf0')
